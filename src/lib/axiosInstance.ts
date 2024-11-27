14ca6ad5f90fe64dc47e090ac1e6b845f9283161
sora2525 <sky0907a.a@gmail.com>
2024-11-27 23:35:02 +0900

    環境変数テスト

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}`;
 
 export const axiosInstance = axios.create({
     baseURL, 
