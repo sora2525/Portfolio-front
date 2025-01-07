3b784871db78ada7a99f50b7ca85fc94c21588f5
sora2525 <sky0907a.a@gmail.com>
2025-01-07 22:46:49 +0900

    add/LineLogin

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,25 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
+import LineProvider from "next-auth/providers/line";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    LineProvider({
+      clientId: process.env.LINE_CLIENT_ID,
+      clientSecret: process.env.LINE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid profile email",
+          response_type: "code",
+          redirect_uri: `${process.env.AUTH_URL}/api/auth/callback/line`,
+          state:crypto.randomUUID(), 
+        },
+      },
+      checks: ["pkce"],
     }),
   ],
   secret: process.env.AUTH_SECRET, 
