3f3c78b81fe8ed0ecee948e09743076c627998f8
sora2525 <sky0907a.a@gmail.com>
2025-01-27 02:21:31 +0900

    add:PWA設定の追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // 開発環境では無効化
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: false,
-};
+});
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,39 +42,42 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon512_rounded.png"></link>
+        <meta name="theme-color" content="#8936FF" />
         <Script
           src="/live2d/live2dcubismcore.min.js"
           strategy="beforeInteractive"
         />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
 
-        <meta property="og:title" content="タスクエールでがんばるもん！"/>
-        <meta property="og:description" content="タスク管理をもっと楽しく！可愛いキャラクターと一緒にタスクを整理しよう。"/>
-        <meta property="og:url" content="https://www.task-yell.jp"/>
-        <meta property="og:site_name" content="タスクエールでがんばるもん！"/>
-        <meta property="og:locale" content="ja_JP"/>
+        <meta property="og:title" content="タスクエールでがんばるもん！" />
+        <meta property="og:description" content="タスク管理をもっと楽しく！可愛いキャラクターと一緒にタスクを整理しよう。" />
+        <meta property="og:url" content="https://www.task-yell.jp" />
+        <meta property="og:site_name" content="タスクエールでがんばるもん！" />
+        <meta property="og:locale" content="ja_JP" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
-        <RecoilRoot>
-          <AuthLoader>
-            <div className="h-screen w-screen bg-[url('/images/sample.jpg')] bg-cover bg-no-repeat bg-center relative overflow-hidden">
-              <div className="absolute top-0 left-0 w-full z-20">
-                <PageHeader />
-              </div>
-              <div className="flex flex-row-reverse w-full">
-                <div className="absolute z-10 pointer-events-none w-full lg:w-[60%] text-brack">
-                  {children}
+          <RecoilRoot>
+            <AuthLoader>
+              <div className="h-screen w-screen bg-[url('/images/sample.jpg')] bg-cover bg-no-repeat bg-center relative overflow-hidden">
+                <div className="absolute top-0 left-0 w-full z-20">
+                  <PageHeader />
                 </div>
-                <div className="absolute inset-0 z-0">
-                  <Live2dView />
+                <div className="flex flex-row-reverse w-full">
+                  <div className="absolute z-10 pointer-events-none w-full lg:w-[60%] text-brack">
+                    {children}
+                  </div>
+                  <div className="absolute inset-0 z-0">
+                    <Live2dView />
+                  </div>
                 </div>
               </div>
-            </div>
-          </AuthLoader>
-        </RecoilRoot>
+            </AuthLoader>
+          </RecoilRoot>
         </SessionProvider>
       </body>
     </html>
