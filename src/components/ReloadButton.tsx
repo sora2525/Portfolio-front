c69a36ba5c6e8ce3517be09dbfc1882ed67c1a08
sora2525 <sky0907a.a@gmail.com>
2025-01-29 20:50:39 +0900

    add/リロードボタンの追加

---
+++ src/components/ReloadButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ReloadButton() {
+    const [fontSize, setFontSize] = useState("28px"); // 初期値
+
+    useEffect(() => {
+        const updateFontSize = () => {
+            if (window.innerWidth < 640) {
+                setFontSize("28px"); // モバイル
+            } else {
+                setFontSize("48px"); // PC
+            }
+        };
+
+        updateFontSize();
+
+        window.addEventListener("resize", updateFontSize);
+        return () => window.removeEventListener("resize", updateFontSize);
+    }, []);
+
+    return (
+        <button onClick={() => window.location.reload()} className="flex flex-col items-center">
+            <span className="material-icons" style={{ fontSize }}>
+                cached
+            </span>
+            <p>リロード</p>
+        </button>
+    );
+}
